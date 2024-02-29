ed370eacd692b832314e425aa7c6d25a33f77069
blazion09 <muhd09afiq@gmail.com>
2024-02-29 19:04:51 +0800

    complete logic for rating result
    add data-score attribute to scoring button and score is recorded to #rating to be shown

--- 03_interactive-rating-component-main/script.js
+++ 03_interactive-rating-component-main/script.js
@@ -2,6 +2,7 @@ let beforeRatingState = document.getElementById("beforeRating");
 let afterRatingState = document.getElementById("afterRating");
 
 const submitBtn = document.getElementById("submitBtn");
+
 //submit btn change the beforeRatingState class from show to hidden
 //also change afterRatingState class from hidden to show
 submitBtn.addEventListener("click", function () {
@@ -12,4 +13,12 @@ submitBtn.addEventListener("click", function () {
   afterRatingState.classList.add("show");
 });
 
-const ratingBtn = document.querySelectorAll("button[score]");
+const ratingBtn = document.querySelectorAll("button[data-score]");
+const ratingResult = document.getElementById("rating");
+
+ratingBtn.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    const score = btn.getAttribute("data-score");
+    ratingResult.textContent = score;
+  });
+});
