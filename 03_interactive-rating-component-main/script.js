8f8be3d0128aeff0eaffbefbc75e969b26fc02f8
blazion09 <muhd09afiq@gmail.com>
2024-02-29 16:12:31 +0800

    add function to change the card state
    use css display hidden

--- 03_interactive-rating-component-main/script.js
+++ 03_interactive-rating-component-main/script.js
@@ -2,6 +2,14 @@ let beforeRatingState = document.getElementById("beforeRating");
 let afterRatingState = document.getElementById("afterRating");
 
 const submitBtn = document.getElementById("submitBtn");
+//submit btn change the beforeRatingState class from show to hidden
+//also change afterRatingState class from hidden to show
 submitBtn.addEventListener("click", function () {
-  document.body.appendChild(afterRatingState);
+  beforeRatingState.classList.remove("show");
+  beforeRatingState.classList.add("hidden");
+
+  afterRatingState.classList.remove("hidden");
+  afterRatingState.classList.add("show");
 });
+
+const ratingBtn = document.querySelectorAll("button[score]");
